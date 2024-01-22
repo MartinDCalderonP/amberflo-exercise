b6c599ec016f6c172464d79d96ad268048f2638a
Martín <martincalderon92@outlook.com>
2024-01-22 18:11:40 -0300

    Update typography in HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import { Typography, Container } from '@mui/material'
 const HomePage = () => {
   return (
     <Container>
-      <Typography variant='h1' align='center'>
+      <Typography
+        variant='h1'
+        align='center'
+        fontSize={40}
+        gutterBottom
+        sx={{
+          marginTop: '2rem'
+        }}
+      >
         Amberflo Exercise
       </Typography>
     </Container>
