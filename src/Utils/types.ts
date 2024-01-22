8da5213851e7564bec35e297be29b3d501a8874d
Martín <martincalderon92@outlook.com>
2024-01-22 17:29:42 -0300

    Add types.ts file for defining Meter interface

---
+++ src/Utils/types.ts
@@ -0,0 +1,12 @@
+type MeterType = 'sum' | 'max' | 'unique_count'
+
+export interface Meter {
+  id: string
+  api_name: string
+  display_name: string
+  active: boolean
+  used_for_billing: boolean
+  type: MeterType
+  updated_time: string
+  created_time: string
+}
